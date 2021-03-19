521aa9f462626fa6a657a0026fe1b1363620d270
BrianGMcEnery <61796887+BrianGMcEnery@users.noreply.github.com>
2021-03-19 18:45:24 +0000

    Duplex's and squares of monomials

--- notebooks/vedmath/vpolynomials.py
+++ notebooks/vedmath/vpolynomials.py
@@ -1,3 +1,5 @@
+from .vnumbers import VInteger
+
 class VMonomial:
     '''
     VMonomial to represent a monomial in vedmath.
@@ -19,4 +21,37 @@ class VMonomial:
 
     def __setitem__(self, key, value):
         self.coeff[key] = value
+
+    def duplex(self):
+        '''
+        Returns the duplex of a VMonomial as a VInteger.
+        '''
+        coeff = self.coeff
+        ld = len(coeff)
+        ans = VInteger(0)
+        for i in range(0, ld // 2):
+            ans = ans + coeff[i] * coeff[-1-i]
+        ans = ans * VInteger(2)
+        if ld % 2:
+            middle = (ld // 2)
+            ans = ans + (coeff[middle] * coeff[middle])
+        return ans
+
+    def square(self):
+        '''
+        Returns the square of a VMonomial as a VMonomial using the duplex.
+        '''
+        coeff = self.coeff
+        ld = len(coeff)
+        ans = []
+
+        for i in range(1, ld + 1):
+            duplex = VMonomial(coeff[:i]).duplex()
+            ans.append(duplex)
+        
+        for i in range(1, ld):
+            duplex = VMonomial(coeff[i:]).duplex()
+            ans.append(duplex)
+            
+        return ans
 
