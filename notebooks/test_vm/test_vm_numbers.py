d6ee2ef1444f3613d16e86984acb49c5eb04c4e2
BrianGMcEnery <61796887+BrianGMcEnery@users.noreply.github.com>
2022-06-25 17:13:46 +0100

    Starting to develop the digit class

--- notebooks/test_vm/test_vm_numbers.py
+++ notebooks/test_vm/test_vm_numbers.py
@@ -1,6 +1,16 @@
-from vm import int_to_digits
+import random
+from vm import int_to_digits, VDigit, digit_from_vdigit
+
+random.seed(0)
 
 def test_int_to_digits():
     assert int_to_digits(345) == [3,4,5]
     assert int_to_digits(0) == [0]
     assert int_to_digits(-345) == [-3, -4, -5]
+
+
+class Test_VDigit:
+    def test_creation(self):
+        assert digit_from_vdigit(VDigit(7)) == 7
+        assert digit_from_vdigit(VDigit(0)) == 0
+        assert digit_from_vdigit(VDigit(-3)) == -3
--- notebooks/vm/vm_numbers.py
+++ notebooks/vm/vm_numbers.py
@@ -20,3 +20,35 @@ def int_to_digits(n):
         digits = [-d for d in digits]
     digits.reverse()
     return digits
+
+def digit_from_vdigit(vd):
+    '''
+    Return a digit from a vdigit.
+    '''
+    if (type(vd) == VDigit):
+        return vd.d
+    else:
+        raise ValueError(f"{vd} is not a VDigit.")
+
+def digits_from_vdigits(vds):
+    '''
+    Return a list of digits from a list of vdigits.
+    '''
+    return [digit_from_vdigit(vd) for vd in vds]
+
+class VDigit:
+    """
+    Root class for all digits in vedmath.
+    """
+    
+    def __init__(self, d):
+        if d in range(-9, 10):
+            self.d = d
+        else:
+            raise ValueError(f'{d} is not a digit.')
+
+    def __str__(self):
+        return f"VDigit({self.d})"
+
+    def __repr__(self):
+        return f"{self.d}"
