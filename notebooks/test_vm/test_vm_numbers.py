242194dc36346d42a63a60c0dbaae4d0b2ff9621
BrianGMcEnery <61796887+BrianGMcEnery@users.noreply.github.com>
2022-06-27 23:55:04 +0100

    trying to get pytest to import properly

--- notebooks/test_vm/test_vm_numbers.py
+++ notebooks/test_vm/test_vm_numbers.py
@@ -1,4 +1,5 @@
 import random
+
 from vm import (int_to_digits, negate_digits, all_from_9_last_from_10, 
     find_truth_changes, to_vinculum, from_vinculum, 
     digits_from_vdigits, VDigit, VInt)
