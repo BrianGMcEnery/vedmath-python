0956b805bab5cda84a4d0e290e10d137cc0bbb2d
BrianGMcEnery <61796887+BrianGMcEnery@users.noreply.github.com>
2022-06-27 23:51:03 +0100

    Deleted old material disrupting operation of pytest. Will fix tomorrow.

--- notebooks/vedmath/__init__.py
+++
@@ -1,5 +0,0 @@
-from .vnumbers import (int_to_digits, VDigit, digit_from_vdigit, 
-                       digits_from_vdigits, VInteger)
-from .voperators import VMul, VDiv, VProp
-from .vpolynomials import VMonomial
-from .vexp import VInt
