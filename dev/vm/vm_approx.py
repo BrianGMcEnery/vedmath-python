6468d0c9afe37ed5b4b6bb9ff9db3b806ebc42d1
BrianGMcEnery <61796887+BrianGMcEnery@users.noreply.github.com>
2022-07-12 00:03:26 +0100

    changes to the approximtion

--- dev/vm/vm_approx.py
+++ dev/vm/vm_approx.py
@@ -1,6 +1,6 @@
+import math
 EPSILON = 1e-10
 
 def approx_equal(a, b, epsilon=EPSILON):
     '''Return true if a and b are approximately equal.'''
-    return(abs(a - b) <= epsilon)
-
+    return math.isclose(a, b, abs_tol=epsilon)
