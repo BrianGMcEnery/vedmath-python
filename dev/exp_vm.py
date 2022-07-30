d124751fa1b3922061e3f4e333da31e55bd1d550
BrianGMcEnery <61796887+BrianGMcEnery@users.noreply.github.com>
2022-07-30 06:31:37 +0100

    Some Sympy experiments

--- dev/exp_vm.py
+++ dev/exp_vm.py
@@ -1,11 +1,3 @@
-from math import sqrt, gcd
-from vm import (Triple, Quadruple, CodeNumber, QuadCodeNumber, Quintuple,
-                code_number_of, triple_of, quad_code_number_of, quadruple_of,
-                sub_quadruples)
+from vm import Triple
 
-qcn = quad_code_number_of(Quadruple(-1, 0, 0, 1))
-
-q = quadruple_of(QuadCodeNumber(1, 0, 1))
-
-diff = sub_quadruples(Quadruple(2, 3, 6, 7), Quadruple(2, 2, 1, 3))
-
+help(Triple)
